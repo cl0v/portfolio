b9044127ab7bd2f950ac257363454d358c5ffea1
Marcelo Viana <marcelofv12@hotmail.com>
2024-10-08 17:49:05 -0300

    add analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,13 @@
 import "./globals.css";
 import { GoogleTagManager } from '@next/third-parties/google'
+import { Analytics } from "@vercel/analytics/react"
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>{children}
+      <Analytics />
+      </body>
       <GoogleTagManager gtmId="16600683276" />
     </html>
   )
